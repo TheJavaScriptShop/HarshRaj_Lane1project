6d9f8fbd32f963fefb201a46c68cc25003026610
TJS3 <tjs3@PriyankOldMBP.local>
2021-03-12 13:01:18 +0530

    integated pdf-generator module

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express=require('express');
 const app= express()
 
+var pdf=require("pdf-creator-node");
+var fs=require("fs");
+var path=require("path");
+var html=fs.readFileSync(path.join(__dirname,"./public/template.html"),"utf8");
+
+
+
+
 const dotenv=require('dotenv');
 dotenv.config();
 const port=process.env.PORT;
@@ -11,6 +19,53 @@ var urlencodedParser = bodyparser.urlencoded({ extended: false })
 
 app.use(express.static('public'))
 
+
+var options={
+    format:"A3",
+    orientation:"potrait",
+    border:"10mm"
+};
+
+//var users=[
+    //{
+      //  name:"jayanth",
+        //age:"22"
+  //  }
+//];
+
+
+// var document={
+//     html:html,
+//     data:{
+//       users:users
+//     },
+//     header: {
+//         height: "45mm",
+//         contents: '<div style="text-align: center;">Harsh</div>'
+//     },
+//     footer: {
+//         height: "28mm",
+//         contents: {
+//             first: 'Cover page',
+//             2: 'Second page', // Any page number is working. 1-based index
+//             default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+//             last: 'Last Page'
+//         }
+//     },
+//     path:"./output/output.pdf",
+//     type:"pdf"
+// };
+
+
+// console.log(document);
+
+// pdf.create(document,options)
+// .then((res)=>{
+// console.log(res)
+// })
+// .catch((error)=>{
+// console.log(error)
+// })
 
 app.get('/form',(req,res)=>{                                //api to render form
     res.sendFile(path.join(__dirname,'public'))
@@ -23,10 +78,50 @@ app.post('/response',urlencodedParser,(req,res)=>{          //api to get respons
     {
         res.send("missing details")
     }
-    
+    var users=[
+        {
+            name:empname,
+            empid:empid,
+            month:month,
+            salary:salary
+        }
+    ];
+    var document={
+        html:html,
+        data:{
+          users:users
+        },
+        header: {
+            height: "45mm",
+            contents: '<div style="text-align: center;">Harsh</div>'
+        },
+        footer: {
+            height: "28mm",
+            contents: {
+                first: 'Cover page',
+                2: 'Second page', // Any page number is working. 1-based index
+                default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+                last: 'Last Page'
+            }
+        },
+        path:"./output/Payslip.pdf",
+        type:"pdf"
+    };
+    console.log(document.data.users);
+
+pdf.create(document,options)
+.then((res)=>{
+console.log(res);
+
+})
+.catch((error)=>{
+console.log(error)
+})
+
     console.log(req.body);
     console.log(empname);
     res.status(200).send("your request has been accepted")
+   // res.sendFile(path.join(__dirname,'output'))
 })
 
 
