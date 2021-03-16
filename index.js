e2312d088d2427ae766bd48e902299a8807de0e7
TJS3 <tjs3@PriyankOldMBP.local>
2021-03-16 10:29:08 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
 const express=require('express');
 const app= express()
-
-var pdf=require("pdf-creator-node");    //pdf-creator npm module
-var fs=require("fs");
-var path=require("path");
-var html=fs.readFileSync(path.join(__dirname,"./public/template.html"),"utf8");
-
-const dotenv=require('dotenv');        //for environmental variable
+const dotenv=require('dotenv');
 dotenv.config();
 const port=process.env.PORT;
 
+const pdf=require("pdf-creator-node");
+const fs=require("fs");
+const path=require("path");
+const html=fs.readFileSync(path.join(__dirname,"./public/template.html"),"utf8");
+
 const bodyparser=require('body-parser')
 app.use(bodyparser.json())
-var urlencodedParser = bodyparser.urlencoded({ extended: false })  
+const urlencodedParser = bodyparser.urlencoded({ extended: false })  
 
 app.use(express.static('public'))
 
-
-var options={                        //options for pdf
-    format:"A3",
-    orientation:"potrait",
-    border:"10mm"
+const options={
+    format: "A4",
+    orientation: "portrait",
+    border: "1mm", 
 };
 
-
-
+app.use(express.static(__dirname + 'public')); //Serves resources from public folder
 
 app.get('/form',(req,res)=>{                                //api to render form
     res.sendFile(path.join(__dirname,'public'))
 })
 
-
 app.post('/response',urlencodedParser,(req,res)=>{          //api to get response
-    const {empname,empid,month,salary}=req.body;
+    let {empname,empid,month,salary}=req.body;
     if(!req.body)
     {
         res.send("missing details")
     }
-    var users=[                                //assigned user data to users object
+    let users=[
         {
             name:empname,
             empid:empid,
@@ -45,51 +41,29 @@ app.post('/response',urlencodedParser,(req,res)=>{          //api to get respons
             salary:salary
         }
     ];
-    var document={                             //document object for pdf
+    let tempFilePath=`./output/Payslip-${empid}-${Math.floor(new Date().getTime() / 1000)}.pdf`;
+    // var tempFilePath=`/Users/tjs3/Documents/pdf_generator/output/Payslip-${empid}-${Math.floor(new Date().getTime() / 1000)}.pdf`;
+    let document={
         html:html,
         data:{
           users:users
         },
-        header: {
-            height: "45mm",
-            contents: '<div style="text-align: center;">Harsh</div>'
-        },
-        footer: {
-            height: "28mm",
-            contents: {
-                first: 'Cover page',
-                2: 'Second page', // Any page number is working. 1-based index
-                default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-                last: 'Last Page'
-            }
-        },
-        path:`./output/Payslip-${empid}-${Math.floor(new Date().getTime() / 1000)}.pdf`,              //path where pdf will be stored
-        type:"pdf"
-    };
-   // console.log(document.data.users);    //for debugging purpose
-
-pdf.create(document,options)          //function which create pdf
-.then((res)=>{
-console.log(res);
-
+        path: tempFilePath,
+        type:""
+    };    
+pdf.create(document,options)
+.then(()=>{
+    let files = fs.createReadStream(tempFilePath);
+    res.writeHead(200, 
+        {'Content-disposition': 'attachment; filename=payslip.pdf'}); //here you can specify file name
+    files.pipe(res); 
 })
 .catch((error)=>{
 console.log(error)
 })
 
-    //console.log(req.body);
-    //console.log(empname);
-    res.status(200).send("your request has been accepted")
-   // res.sendFile(path.join(__dirname,'output'))
-})
-
-
-
-// app.get("/",(req,res)=>{
-//     res.send("hello");
-// })
-
+});
 
 app.listen(port,()=>{
     console.log(`Server is running at ${port}`);
-})
+});
