4aea2c76c6ca263a152525f9524ab0ad12f4d4fe
harshrajmishra1995 <harshrajmishra.1995@gmail.com>
2021-03-17 16:11:42 +0530

    modified the code in index.js and template.html

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ const html=fs.readFileSync(path.join(__dirname,"./public/template.html"),"utf8")
 
 const bodyparser=require('body-parser')
 app.use(bodyparser.json())
-const urlencodedParser = bodyparser.urlencoded({ extended: false })  
+const urlencodedParser = bodyparser.urlencoded({ extended: false })
 
 app.use(express.static('public'))
 
 const options={
     format: "A4",
     orientation: "portrait",
-    border: "1mm", 
+    border: "1mm",
 };
 
 app.use(express.static(__dirname + 'public')); //Serves resources from public folder
@@ -28,40 +28,39 @@ app.get('/form',(req,res)=>{                                //api to render form
 })
 
 app.post('/response',urlencodedParser,(req,res)=>{          //api to get response
-    let {empname,empid,month,salary}=req.body;
-    if(!req.body)
-    {
-        res.send("missing details")
-    }
-    let users=[
-        {
-            name:empname,
-            empid:empid,
-            month:month,
-            salary:salary
-        }
-    ];
-    let tempFilePath=`./output/Payslip-${empid}-${Math.floor(new Date().getTime() / 1000)}.pdf`;
-    // var tempFilePath=`/Users/tjs3/Documents/pdf_generator/output/Payslip-${empid}-${Math.floor(new Date().getTime() / 1000)}.pdf`;
-    let document={
-        html:html,
-        data:{
-          users:users
-        },
-        path: tempFilePath,
-        type:""
-    };    
-pdf.create(document,options)
-.then(()=>{
-    let files = fs.createReadStream(tempFilePath);
-    res.writeHead(200, 
-        {'Content-disposition': 'attachment; filename=payslip.pdf'}); //here you can specify file name
-    files.pipe(res); 
-})
-.catch((error)=>{
-console.log(error)
-})
-
+        let {empname,empid,month,salary}=req.body;
+        if(!req.body)
+         {
+          res.send("missing details")
+         }
+        let users=[
+            {
+                name:empname,
+                empid:empid,
+                month:month,
+                salary:salary
+            }
+        ];
+      let pdfFilePath=`./output/Payslip-${empid}-${Math.floor(new Date().getTime() / 1000)}.pdf`;
+     // var tempFilePath=`/Users/tjs3/Documents/pdf_generator/output/Payslip-${empid}-${Math.floor(new Date().getTime() / 1000)}.pdf`;
+        let document={
+            html:html,
+            data:{
+            users:users
+            },
+            path: pdfFilePath,
+            type:""
+        };
+       pdf.create(document,options)
+       .then(()=>{
+            let files = fs.createReadStream(pdfFilePath);
+            res.writeHead(200,
+          {'Content-disposition': 'attachment; filename=payslip.pdf'}); //here you can specify file name
+           files.pipe(res);
+        })
+       .catch((error)=>{
+       console.log(error)
+       })
 });
 
 app.listen(port,()=>{
