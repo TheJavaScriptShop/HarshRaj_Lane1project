621d81c4654769ddc1d5b0f617d3dbed167b8adc
harshrajmishra1995 <harshrajmishra.1995@gmail.com>
2021-04-15 14:16:08 +0530

    updated the code with es6 syntax

--- paymentlogic.js
+++ paymentlogic.js
@@ -1,36 +1,34 @@
 
-function basic(salary)
-{
-    var basicpay=(0.40 * salary)
-    return basicpay;
+let basic=(salary)=>{
+    let basicpay=(0.40 * salary)
+    return basicpay
 }
-function da(salary)
-{
-    const DA=(0.20 * salary)
+
+let da=(salary)=>{
+    let DA=(0.20 * salary)
     return DA;
 }
-function hra(salary)
-{
-    const HRA=(0.20 * salary)
+
+let hra=(salary)=>{
+    let HRA=(0.20 * salary)
     return HRA;
 }
-function special(salary)
-{
-    const Special=(0.20 * salary)
-    return Special;
+
+let special=(salary)=>{
+    let Special=(0.20 * salary)
+    return Special
 }
 
-function amountdata(amt){
-    var z=amt;
-    z=z.toString();
-    var lastThree = z.substring(z.length-3);
-    var otherNumbers = z.substring(0,z.length-3);
+let amountdata=(amt)=>{
+    let amtdata=amt;
+    amtdata=amtdata.toString();
+    let lastThree = amtdata.substring(amtdata.length-3);
+    let otherNumbers = amtdata.substring(0,amtdata.length-3);
     if(otherNumbers != '')
         lastThree = ',' + lastThree;
-    var result= otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
-    return result
+    var finaldata= otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
+    return finaldata
 }
-
 module.exports={
     basic,
     da,
