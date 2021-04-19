b686b1a278f1a23465242916d0482354ab79f04a
harshrajmishra1995 <harshrajmishra.1995@gmail.com>
2021-04-19 17:56:05 +0530

    Adjusted the form view with small devices

--- paymentLogic.js
+++ paymentLogic.js
@@ -1,41 +1,35 @@
-
-
 const basic = (salary) => {
-    let basicpay= (0.40 * salary)
+    let basicpay = (0.40 * salary)
     return basicpay
 }
 
-const da=(salary)=>{
-
-    let DA=(0.20 * salary)
+const da = (salary) => {
+    let DA = (0.20 * salary)
     return DA;
 }
 
-
-const hra=(salary)=>{
-    let HRA=(0.20 * salary)
+const hra = (salary) => {
+    let HRA = (0.20 * salary)
     return HRA;
 }
 
-
-const special=(salary)=>{
-
-    let Special=(0.20 * salary)
+const special = (salary) => {
+    let Special = (0.20 * salary)
     return Special
 }
 
-const amountData=(amt)=>{
+const amountData = (amt) => {
     let amount = amt;
     amount = amount.toString();
     let lastThreeDigits = amount.substring(amount.length-3);
     let otherNumbers = amount.substring(0,amount.length-3);
     if(otherNumbers != '')
         lastThreeDigits = ',' + lastThreeDigits;
-    var finalAmount= otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThreeDigits;
+    let finalAmount = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThreeDigits;
     return finalAmount;
-
 }
-module.exports={
+
+module.exports = {
     basic,
     da,
     hra,
